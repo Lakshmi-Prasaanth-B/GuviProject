2bcbee47f5eab62846642fc73ef680f50a2615b8
Lakshmi-Prasaanth-B <123284565+Lakshmi-Prasaanth-B@users.noreply.github.com>
2024-04-04 07:05:01 +0530

    Updated login.php

--- Guvi/php/login.php
+++ Guvi/php/login.php
@@ -1,41 +1,66 @@
 <?php
-    $con = new mysqli('localhost','root','password','Guvi');
+    header('Content-Type: application/json');
+    $con = new mysqli('localhost','root','King','Guvi');
     if($con->connect_errno){
         echo $con->connect_error;
         die();
     }
     else{
-        
-        if($_SERVER["REQUEST_METHOD"] == "POST"){
-            $email = $_POST['email'];
-            $password = $_POST['password'];
+        if($_SERVER["REQUEST_METHOD"]=="POST"){
+            $rawData = file_get_contents('php://input');
+            parse_str($rawData,$dataArray);
+            $jsonData = json_encode($dataArray);
+            $email = $dataArray['email'];
+            $password = $dataArray['password'];
+
             $flag = 0;
+            $name = '';
             $stmt = "select * from users";
-            $res = $con->query($stmt);
-            if($res-> num_rows > 0){
-                while($row = $res->fetch_assoc()){
-                    if(($email==$row["email"] && $password==$row["password"]) || ($email==$row["name"] && $password==$row["password"])){
+            $resData = $con->query($stmt);
+            if($resData->num_rows>0){
+                while($row = $resData->fetch_assoc()){
+                    if(($email==$row["name"] && $password==$row["password"]) || ($email==$row["email"] && $password==$row["password"])){
                         $flag = 1;
+                        $name = $row["name"];
+                        $email = $row["email"];
+                        $contact = $row["mobile"];
                     }
                 }
                 if($flag==1){
-                    echo "success";
+                    $response = array(
+                        'status' => 'success',
+                        'user' => $name,
+                        'email' => $email,
+                        'contact' => $contact,
+                        'password' => $password,
+                        'message' => 'User Exists',
+                        'error' => 'No Error Encountered'
+                    );
+                    echo json_encode($response);
                 }
                 else{
-                    echo 'not exist';
+                    $response = array(
+                        'status' => 'error',
+                        'email' => $email,
+                        'password' => $password,
+                        'message' => 'User does not Exists or Invalid UserName or Password'
+                    );
+                    echo json_encode($response);
                 }
             }
             else{
-                echo "0 Results Found...";
+                $response = array(
+                    'status' => 'error',
+                    'email' => $email,
+                    'password' => $password,
+                    'message' => 'No Datas Found'
+                );
+                echo json_encode($response);
             }
+            
         }
         else{
-            echo "Request method was not POST<br>";
-            echo "Request Method : ". $_SERVER["REQUEST_METHOD"];
-            var_dump($_POST);
+            echo "Request Method was not POST..!\n";
         }
     }
-
 ?>
-
-
