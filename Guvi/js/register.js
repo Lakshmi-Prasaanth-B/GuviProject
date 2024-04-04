375db3e05ff8bb65269f4dc2f4d773616dd40527
Lakshmi-Prasaanth-B <123284565+Lakshmi-Prasaanth-B@users.noreply.github.com>
2024-04-04 07:11:59 +0530

    Update register Ajax

--- Guvi/js/register.js
+++ Guvi/js/register.js
@@ -1,30 +1,40 @@
-function sub(){
-    e.preventDefault();
-    console.log(s_name.value);
-    console.log(s_email.value);
-    console.log(s_mobile.value);
-    console.log(s_password.value);
-    console.log(s_check_password.value);
-    
-    flag = 0;
-    dummyData.forEach((item)=>{
-        if(item.email==s_email.value && flag==0){
-            s_msg.innerHTML="Email already Exists";
-            flag = 1;
-            setTimeout(()=>{
-                s_msg.innerHTML="";
-            },5000);
-        }
-    })
-    if(flag==0){
-        console.log("Yet to Redirect");
-        s_msg.innerHTML="User Added Successfully";
-        console.log("Yet to Update");
-        setTimeout(()=>{
-            s_msg.innerHTML="";
-            console.log("Location is going to be changed");
-            s_form.submit();
-        },7000);
-        window.location.replace("login.html");
-    }
-}
+console.log("Register Page JS Loaded");
+
+$(()=>{
+    $('#addUser').click(function(ev){
+        ev.preventDefault();
+        let form = $('#register-form');
+        console.log(form);
+        let data = form.serialize();
+        console.log(data);
+
+        $.ajax({
+            type: "POST",
+            url: 'php/register.php',
+            data: data,
+            success:function(response,status,error){
+                alert("Form Submitted Successfully");
+                let JSONdata = JSON.stringify(response);
+                console.log(JSONdata);
+                console.log("Response : "+response);
+
+                var responseData = JSON.parse(JSONdata);
+                if(responseData.status === 'success'){
+                    alert("User Added Successfully");
+                    console.log(responseData.user);
+                    alert(responseData.message)
+                    window.location.replace("index.html");
+                }
+                else if(response.status === 'error'){
+                    alert(responseData.message);
+                }
+            },
+            error:function(response,status,error){
+                console.log("Response : "+response);
+                console.log("Status : "+status);
+                console.log("Error : "+error);
+                alert("There is an Error");
+            }
+        });
+    });
+});
